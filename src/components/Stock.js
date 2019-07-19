b4dfaea2ba9e206f54fec972a0aa3e636ec671d6
rrosztoczy <rrosztoczy@gmail.com>
2019-07-19 17:05:58 -0600

    rendered db data

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = ({ stockData }) => (
   <div>
 
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{
             //Company Name
+           stockData.name
           }</h5>
         <p className="card-text">{
             //ticker: stock price
+           stockData.price
           }</p>
       </div>
     </div>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,6 +5,27 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: {}
+  };
+
+  componentDidMount() {
+    const getAllStocks = async () => {
+        const headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        };
+        const resp = await fetch('http://localhost:3000/stocks', { headers: headers });
+        const jsonData = await resp.json();
+        this.setState({
+          stocks: [...this.state.stocks, ...jsonData]
+        });
+    };
+
+    getAllStocks();
+  };
+
+
   render() {
     return (
       <div>
@@ -13,12 +34,12 @@ class MainContainer extends Component {
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.stocks}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.stocks}/>
 
             </div>
           </div>
